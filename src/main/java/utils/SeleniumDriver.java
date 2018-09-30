f45df957bd9cf6d1c2e85cdd785a0908160a3b46
gowriannapantula <38232339+gowriannapantula@users.noreply.github.com>
2018-10-01 09:50:14 +1000

    Integrating support for Travis-CI

--- src/main/java/utils/SeleniumDriver.java
+++ src/main/java/utils/SeleniumDriver.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class SeleniumDriver {
     public final static int TIMEOUT = 30;
     public final static int PAGE_LOAD_TIMEOUT = 50;
-    static String DefaultBro = "chrome";
+    static String DefaultBrowser = "chrome";
     private static SeleniumDriver seleniumDriver;
     private static WebDriver driver;
     private static WebDriverWait waitDriver;
@@ -80,12 +80,12 @@ public class SeleniumDriver {
         String value = System.getProperty(name);
 
         if (value == null) {
-            value = DefaultBro;
+            value = DefaultBrowser;
             //throw new RuntimeException(name + " is not a parameter!");
         }
 
-        if (value.isEmpty()) {
-            value = DefaultBro;
+        if ("".equals(value)) {
+            value = DefaultBrowser;
             //throw new RuntimeException(name + " is empty!");
         }
 
